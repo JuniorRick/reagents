34b67ac2e6bacd7eb6508085eaeb7ed8c141e289
Erast Stinca <eric.stinca@gmail.com>
2018-07-11 15:21:46 +0300

    scroll bottom bux fix

--- public/js/raw.js
+++ public/js/raw.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
 
   $('.scroll-down').click( function() {
     $("html, body").animate({
-      scrollTop: 100,
+      scrollTop: $(document).height(),
     }, 500);
   });
 
